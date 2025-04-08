049dd0735aa238c8c94e9bafd1b0cf19aeb49de0
RedLime <redlimerl@gmail.com>
2025-04-09 02:42:00 +0900

    Init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,79 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "MCSR Ranked Wiki",
+  description: "Welcome to MCSR Ranked Wiki Page",
+
+  head: [
+    ['link', { rel: 'icon', href: '/icon.png' }]
+  ],
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/icon.png',
+    nav: [
+      { text: 'Install', link: '/install' },
+      { text: 'Gameplay', link: '/gameplay' },
+      { text: 'Playoffs', link: '/playoffs' }
+    ],
+
+    sidebar: {
+      '/gameplay': [
+        {
+          text: 'Gameplay',
+          link: '/',
+          base: '/gameplay',
+          items: [
+            { 
+              text: 'MCSR Ranked Seed', 
+              items: [
+                { text: 'Filtered Seed', link: '/seed' },
+                { text: 'RNG Standardization', link: '/rng' },
+              ]
+            },
+            { text: 'Match-Making', link: '/matchmaking' },
+          ]
+        }
+      ],
+      'playoffs': [
+        { 
+          text: 'Playoffs',
+          link: '/',
+          base: '/playoffs',
+          items: [
+            {
+              text: 'Phase Point',
+              items: [
+                { text: 'Phase Point System', link: '/phase_point' },
+                { text: 'Midseason Showdown', link: '/midseason_showdown' }
+              ]
+            }
+          ]
+        },
+      ]
+    },
+
+    socialLinks: [
+      { 
+        icon: { svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7z"/></svg>' }, 
+        link: 'https://mcsrranked.com',
+        ariaLabel: 'Back to Homepage'
+      },
+      { icon: 'github', link: 'https://github.com/MCSR-Ranked/Wiki', ariaLabel: 'Wiki Repository' },
+      { icon: 'discord', link: 'https://mcsrranked.com/discord', ariaLabel: 'Discord' },
+      { icon: 'x', link: 'https://mcsrranked.com/x', ariaLabel: "X (Twitter)" },
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/MCSR-Ranked/Wiki/edit/main/docs/:path'
+    },
+
+    search: {
+      provider: 'local'
+    },
+  },
+
+  cleanUrls: true,
+  lastUpdated: true
+})
---
+++ docs/playoffs/phase_point.md
@@ -0,0 +1,22 @@
+# Phase Point System
+
+## System Explanation
+Each season is split into **4**, month-long, phases. You will only need to do placement games at the beginning of each season; new phases will not impact your elo. These phases serve to qualify for the MCSR Ranked Playoffs at the end of each season.
+
+Your placement at the end of each phase will reward you with a certain amount of **Phase Points**, which will be displayed on your profile. At the end of the season, the Top 12 players with the most points will be automatically invited to the bracket stage of the MCSR Ranked Playoffs. In the event of a tie in phase points, the higher seeding will go to the player with the higher final season elo. All players with 20 or more points will be invited to a Last-Chance Qualifier to compete for the remaining 4 bracket positions.
+
+As the phases progress, you can receive more and more points, rewarding players who play consistently throughout the season. We may also sporadically run smaller events with the opportunity to earn extra points. 
+
+## Point Breakdowns
+
+| Position      | Phase 1 | Phase 2 | Phase 3 | Phase 4 |
+| :------:      | :-----: | :-----: | :-----: | :-----: |
+| 1st           | 50 | 60 | 75 | 95 |
+| 2nd           | 40 | 50 | 65 | 85 |
+| 3rd           | 35 | 40 | 55 | 70 |
+| 4th           | 30 | 35 | 50 | 65 |
+| 5th to 8th    | 25 | 30 | 40 | 45 |
+| 9th to 16th   | 20 | 25 | 30 | 35 |
+| 17th to 32th  | 15 | 20 | 20 | 25 |
+| 33rd to 50th  | 10 | 15 | 15 | 20 |
+| 51st to 100th | 5  | 10 | 10 | 15 |
