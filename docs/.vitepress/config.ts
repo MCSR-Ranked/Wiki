0e8fb3e05ea3b22f61265bc70bc8a7cc312b4284
Slayer 2 <94136016+MortalSlayer24@users.noreply.github.com>
2025-04-09 12:30:32 +0100

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -57,6 +57,7 @@ export default defineConfig({
             { text: 'Private Rooms', link: '/private_rooms' },
             { text: 'Weekly Race', link: '/weekly_race' },
             { text: 'Achievements', link: '/achievements' },
+            { text: 'Replay System', link: '/replay_system' },
           ]
         }
       ],
